0cea66582c8b81a3f30ad84f3d8bd5f72152780a
JDM-Github <jdmaster888@gmail.com>
2024-09-01 12:22:07 +0800

    Test Update

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-# from kivy.config import Config
-# WIDTH  = int(720 * 0.5)
-# HEIGHT = int(1400 * 0.5)
-# Config.set('graphics', 'width', WIDTH)
-# Config.set('graphics', 'height', HEIGHT)
-# # Config.set('graphics', 'resizable', False)
-# Config.write()
+from kivy.config import Config
+WIDTH  = int(720 * 0.5)
+HEIGHT = int(1400 * 0.5)
+Config.set('graphics', 'width', WIDTH)
+Config.set('graphics', 'height', HEIGHT)
+Config.set('graphics', 'resizable', False)
+Config.write()
 
 from kivy.app import App
-# from kivy.uix.image import Image
+from kivy.uix.image import Image
 from kivy.uix.label import Label
 
 class MainApp(App):
 
     def build(self):
-        return Label(text="assets/JDMBG.png")
+        return Image(source="assets/JDMBG.png")
 
 if __name__ == "__main__":
     MainApp().run()
