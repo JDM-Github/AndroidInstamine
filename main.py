80550222f5b637557beb5dc32379d6f9b59469f9
JDM-Github <jdmaster888@gmail.com>
2024-09-01 12:17:28 +0800

    Test Update

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from kivy.config import Config
-WIDTH  = int(720 * 0.5)
-HEIGHT = int(1400 * 0.5)
-Config.set('graphics', 'width', WIDTH)
-Config.set('graphics', 'height', HEIGHT)
-Config.set('graphics', 'resizable', False)
-Config.write()
+# from kivy.config import Config
+# WIDTH  = int(720 * 0.5)
+# HEIGHT = int(1400 * 0.5)
+# Config.set('graphics', 'width', WIDTH)
+# Config.set('graphics', 'height', HEIGHT)
+# # Config.set('graphics', 'resizable', False)
+# Config.write()
 
 from kivy.app import App
 # from kivy.uix.image import Image
